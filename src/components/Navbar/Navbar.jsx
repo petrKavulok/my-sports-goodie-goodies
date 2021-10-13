b43db880124ac9e2957992de1d43c08492d3e394
petrKavulok <petr.kavulok@gmail.com>
2021-10-14 01:06:34 +0200

    commerce.js initiation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Appbar, Toolbar, IconButton, Badge, MenuItem, Item, Menu, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 
 import logo from '../../assets/pngegg.png';
 import useStyles from './styles';
 
-function NavbarNav() {
+function Navbar() {
 
-    const styles = useStyles();
+    const classes = useStyles();
 
     return (
         <>
-            <Appbar position="fixed" className={classes.appBar} colo="inherit">
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
                     <Typography variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="My Sports Goodie Goodies" height="25px" className={classes.image} />
@@ -27,9 +27,9 @@ function NavbarNav() {
                     </div>
                 </Toolbar>
 
-            </Appbar>
+            </AppBar>
         </>
     )
 }
 
-export default NavbarNav
+export default Navbar
